0ad728165cc3e9f13d2eb07011ed6c8cc31da2f1
alschofield93 <alschofield93@gmail.com>
2021-02-19 18:36:35 -0500

    nav changes

--- src/features/main-components/nav/Nav.js
+++ src/features/main-components/nav/Nav.js
@@ -7,7 +7,8 @@ const Nav = ({ theme, handleThemeToggle }) => {
   return (
     <Container theme={theme.nav.container} data-testid='nav-container'>
       <NavItem theme={theme.nav.item} type='image' src={`${process.env.PUBLIC_URL}/scho-logo.png`} href='/' />
-      {/* <NavItem type="anchor" value="Merch" href="https://scho.clothing" /> */}
+      <NavItem theme={theme.nav.item} type="anchor" value="Calculator" href="https://git.heroku.com/enigmatic-fjord-59171.git" />
+      <NavItem theme={theme.nav.item} type="anchor" value="Todos App" href="https://still-brushlands-04490.herokuapp.com/" />
       <NavItem theme={theme.nav.item} type='icon' icon='paint' onClick={handleThemeToggle} />
     </Container>
   );
