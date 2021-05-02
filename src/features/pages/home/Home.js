0df210f29a42a04f6ae7bcf7b60e1fc60ec62dff
alschofield93 <alschofield93@gmail.com>
2021-05-01 21:05:07 -0400

    updates

--- src/features/pages/home/Home.js
+++ src/features/pages/home/Home.js
@@ -7,13 +7,13 @@ const Home = ({ theme }) => {
   return (
     <Container theme={theme.home.container} data-testid='home-container'>
       <Header>Current Projects</Header>
-      <List list={['add tests to web apps', 'npm publish components and ingest in apps', 'learn rust (programming language)', 'finish up nft coin']} />
+      <List list={['learn rust (programming language)', 'auth server']} />
       <Header>Current Projects - Long Term</Header>
       <List list={['study math']} />
       <Header>Projects on Hold</Header>
       <List list={['DWC (direct water capture) (need to study more math and engineering first)', 'study engineering (want to refresh/learn more math first)']} />
       <Header>Future Projects</Header>
-      <List list={['improvements on portfolio style and ideas for adding more stuff or flashy styling', 'aws lambda', 'pontificate on the potential of a podcast/talk show/interview thing', 'pontificate on wearables ive thought up in the past and maybe some new ones', 'pontificate on DWC and climate change in general more', 'learn assembly language', 'learn c', 'learn java']} />
+      <List list={['proxy server', 'improvements on portfolio style and ideas for adding more stuff or flashy styling', 'finish up nft coin', 'pontificate on the potential of a podcast/talk show/interview thing', 'pontificate on wearables ive thought up in the past and maybe some new ones', 'pontificate on DWC and climate change in general more', 'learn assembly language', 'learn c', 'learn java']} />
     </Container>
   );
 }
