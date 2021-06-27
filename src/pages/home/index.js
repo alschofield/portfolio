9f33755095c84f908596cbfc962c22515c7c05b1
alschofield93 <alschofield93@gmail.com>
2021-06-27 00:31:11 -0400

    scroll snaps

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,17 +4,33 @@ import { Container, Header, List } from 'schofield-common-components';
 import { withTheme } from 'schofield-themes';
 
 const Home = ({ theme }) => {
+
+
   return (
     <Container theme={theme.home.container} data-testid='home-container'>
-      <Header>Current Projects</Header>
-      <List list={['implement auth in nav and apps']} />
-      <Header>Current Projects - Long Term</Header>
-      <List list={['learn rust (programming language)', 'study math']} />
-      <Header>Projects on Hold</Header>
-      <List list={['DWC (direct water capture) (need to study more math and engineering first)', 'study engineering (want to refresh/learn more math first)']} />
-      <Header>Future Projects</Header>
-      <List list={['improvements on portfolio style and ideas for adding more stuff or flashy styling', 'finish up nft coin', 'pontificate on the potential of a podcast/talk show/interview thing', 'pontificate on wearables ive thought up in the past and maybe some new ones', 'pontificate on DWC and climate change in general more', 'learn assembly language', 'learn c', 'learn java']} />
-    </Container>
+      <Container theme={{ ...theme.home.section, ...theme.home.reccomendations }}>
+        
+      </Container>
+      <Container theme={{ ...theme.home.section, ...theme.home.cryptunes }}>
+        
+      </Container>
+      <Container theme={{ ...theme.home.section, ...theme.home.proxy }}>
+        
+      </Container>
+      <Container theme={{ ...theme.home.section, ...theme.home.dwc }}>
+        
+      </Container>
+      <Container theme={{ ...theme.home.section, ...theme.home.updates }}>
+        <Header>Current Projects</Header>
+        <List list={['implement auth in nav and apps']} />
+        <Header>Current Projects - Long Term</Header>
+        <List list={['learn rust (programming language)', 'study math']} />
+        <Header>Projects on Hold</Header>
+        <List list={['DWC (direct water capture) (need to study more math and engineering first)', 'study engineering (want to refresh/learn more math first)']} />
+        <Header>Future Projects</Header>
+        <List list={['improvements on portfolio style and ideas for adding more stuff or flashy styling', 'finish up nft coin', 'pontificate on the potential of a podcast/talk show/interview thing', 'pontificate on wearables ive thought up in the past and maybe some new ones', 'pontificate on DWC and climate change in general more', 'learn assembly language', 'learn c', 'learn java']} />
+      </Container>
+  </Container>
   );
 }
 
